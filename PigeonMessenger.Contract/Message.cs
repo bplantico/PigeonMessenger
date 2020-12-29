278b4fb62858353c0ab5c78031d31bc5306e5e01
Brian Plantico <bplantico@gmail.com>
2020-12-29 12:05:27 -0700

    Fixes path to look for tests and adds comments to Message.cs

--- PigeonMessenger.Contract/Message.cs
+++ PigeonMessenger.Contract/Message.cs
@@ -5,10 +5,10 @@ namespace PigeonMessenger.Contract
     public class Message
     {
         public string Id { get; set; }
-        public string Sender { get; set; }
+        public string Sender { get; set; } // Really this could be an id as an int, long, guid, etc and be named SenderId. While seeing this in action, I wanted to see the names of the sender and receiver.
         public string Recipient { get; set; }
         public string Body { get; set; }
-        public bool IsPublic { get; set; } = true;
+        public bool IsPublic { get; set; } = true; // Perhaps later there's a feature (premium/tiered) that users can send private messages that can't be retrieved by the rest of the world?
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
     }
